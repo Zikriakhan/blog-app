257748ca7040c4a8b22e62e06323d5303c027b4d
Zikriakhan <muhammadjanzikria@gmail.com>
2023-07-31 02:25:34 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './BlogApp/App';
+// ssimport reportWebVitals from './reportWebVitals';
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App
+    />
+);
+
+// reportWebVitals();
